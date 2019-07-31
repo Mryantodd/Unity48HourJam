d1a52a8cfa3509643713f440a273e7ac9e206589
Matthew Ryan Todd <42684175+Mryantodd@users.noreply.github.com>
2019-07-31 00:56:36 -0500

    Adding overview notation to Stats.cs
    Adding overview notation to Stats.cs

--- C#source/Stats.cs
+++ C#source/Stats.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections;
+// A simple data container type to be attached to objects at run time when building out UI elements. I redeclared this enum in 
+// the statholderscript.cs.. Any changes here require updates to that one as well. I wanted to avoid this. However each script would
+// then require the xmlobject reference. Something I'll likely change at some point.
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
