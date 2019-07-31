fa260f5c2149eaf42d41559eb6539d850c2df6da
Matthew Ryan Todd <42684175+Mryantodd@users.noreply.github.com>
2019-07-30 22:43:43 -0500

    Adding overview notation to MyMenuScript.cs
    Adding overview notation to MyMenuScript.cs

--- C#source/MyMenuScript.cs
+++ C#source/MyMenuScript.cs
@@ -1,16 +1,21 @@
-﻿using System.Collections;
+﻿/// The following script acts as a controller for my main UI panels. Each section of the application is added to the list via Inspector.
+/// By keeping each panel's canvasgroup component in reference I am able to set one as active. Then Cycle through the list setting each 
+/// panel to the appropriate state invis/no click and visa versa.
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MyMenuScript : MonoBehaviour {
-    public List<CanvasGroup> MyMenus = new List<CanvasGroup>();
-    public CanvasGroup SelectedCanvas;
+    public List<CanvasGroup> MyMenus = new List<CanvasGroup>();/// Add each panel to the list in inspector to track.
+    public CanvasGroup SelectedCanvas; /// This is set in inspector to default the main panel. After which it dynamicly updates on button use.
 
+    /// Set up all panels.
     public void Start()
     {
         MakeSelectedVisible();
     }
-
+    
+    /// Method that sanity checks we are not mashing the same button. Then loads a new canvas group.
     public void SetSelectedTool(CanvasGroup a)
         
     {
@@ -20,6 +25,7 @@ public class MyMenuScript : MonoBehaviour {
             MakeSelectedVisible();
         }
     }
+    /// The following will loop through all panels. If not the currenly selected panel. They become invis/no click. else visa versa.
     public void MakeSelectedVisible()
     {
         foreach (CanvasGroup b in MyMenus)
@@ -39,7 +45,7 @@ public class MyMenuScript : MonoBehaviour {
 
         }
     }
-
+    /// method called externally to finally close application if exit button was clicked.
     public void ExitApp()
     {
         Application.Quit();
