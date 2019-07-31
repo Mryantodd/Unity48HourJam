bc4c6113cf151ea0c370bc35ed2bab903863af35
Matthew Ryan Todd <42684175+Mryantodd@users.noreply.github.com>
2019-07-30 22:16:51 -0500

    Adding overview notation to DotScript.cs
    Adding overview notation to DotScript.cs

--- C#source/DotScript.cs
+++ C#source/DotScript.cs
@@ -1,4 +1,12 @@
-﻿using System.Collections;
+/// This is a fairly simple type definition. The DotScript type is referened throughout the project.
+
+/// It is attached to every Dot and Square UI element while identifying its children components for direct access.
+/// The major function of this script is to make each dot and square easily accessed. As each consist of 2 images.
+/// A hollow circle/square layed over a solid one. To give the elements the correct look for point distribution.
+
+/// This is how I cycle through each UI element to set their inner/outer state during I/O operations.
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,7 +16,7 @@ public class DotScript : MonoBehaviour {
     public Image InDot;
     public bool isDot = true;
     public bool isSquare = false;
-	// Use this for initialization
+	// Here we use awake to ensure this data is available in the initialization steps of other scripts.
 	void Awake () {
         if (isDot)
         {
